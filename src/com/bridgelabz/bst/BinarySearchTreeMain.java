2c06141dcd3c96cfaf99e960a38231f25ef72f69
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-01 22:22:51 +0530

    ADDED:class that provides main method

---
+++ src/com/bridgelabz/bst/BinarySearchTreeMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.bst;
+
+public class BinarySearchTreeMain {
+	 public static void main(String[] args) {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		
+		System.out.println("inorder traversal is:");
+		myBinaryTree.inorderTraversal();
+	}
+}

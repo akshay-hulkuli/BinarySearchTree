19a274092fa697917c264c4a430e7b92ba89c1ef
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-01 22:31:31 +0530

    ADDED: call to size method

--- src/com/bridgelabz/bst/BinarySearchTreeMain.java
+++ src/com/bridgelabz/bst/BinarySearchTreeMain.java
@@ -6,8 +6,21 @@ public class BinarySearchTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(70);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		
 		System.out.println("inorder traversal is:");
 		myBinaryTree.inorderTraversal();
+		System.out.println();
+		System.out.println("the size of tree is :"+ myBinaryTree.getSize());
 	}
 }
